aea04632234c8cb9a4bede6ea8d54050d1ccac1f
iuryAmicussi <vapo_iury@hotmail.com>
2018-01-29 10:43:34 -0200

    Add project files.

---
+++ ProjetosPessoais.Baguim.UI.WindowsUtil/ExtensionUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetosPessoais.Baguim.UI.WindowsUtil
+{
+    public static class ExtensionUtils
+    {
+        public static void OnHover(this object self, Color corNomal, Color corOnHover)
+        {   
+            if (self is Label)
+            {
+                ((Label)self).MouseEnter += (sender,EventArgs) => Label_MouseEnter(sender,EventArgs,corOnHover);
+                ((Label)self).MouseLeave += (sender,EventArgs) => Label_MouseLeave(sender,EventArgs,corNomal);
+            }
+        }
+
+        private static void Label_MouseLeave(object sender, EventArgs e,Color corNomal)
+        {
+            ((Label)sender).BackColor = corNomal;
+        }
+
+        private static void Label_MouseEnter(object sender, EventArgs e,Color corOnHover)
+        {
+            ((Label)sender).BackColor = corOnHover;
+        }
+    }
+
+
+}
---
+++ ProjetosPessoais.Baguim.UI/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace ProjetosPessoais.Baguim.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel_Geral = new System.Windows.Forms.Panel();
+            this.panel_Container = new System.Windows.Forms.Panel();
+            this.panel_BarraDeJanelas = new System.Windows.Forms.Panel();
+            this.panel_MenuSuperior = new System.Windows.Forms.Panel();
+            this.menu_Principal = new System.Windows.Forms.MenuStrip();
+            this.açõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minimizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fecharToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emitirNotaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.receberNotaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grupoDeProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fornecedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel_BarraSuperior = new System.Windows.Forms.Panel();
+            this.label_MinimizarSistema = new System.Windows.Forms.Label();
+            this.label_FecharSistema = new System.Windows.Forms.Label();
+            this.panel_LogoEMenuLateral = new System.Windows.Forms.Panel();
+            this.panel_MenuLateral = new System.Windows.Forms.Panel();
+            this.panel_Logo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_Geral.SuspendLayout();
+            this.panel_MenuSuperior.SuspendLayout();
+            this.menu_Principal.SuspendLayout();
+            this.panel_BarraSuperior.SuspendLayout();
+            this.panel_LogoEMenuLateral.SuspendLayout();
+            this.panel_Logo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_Geral
+            // 
+            this.panel_Geral.Controls.Add(this.panel_Container);
+            this.panel_Geral.Controls.Add(this.panel_BarraDeJanelas);
+            this.panel_Geral.Controls.Add(this.panel_MenuSuperior);
+            this.panel_Geral.Controls.Add(this.panel_LogoEMenuLateral);
+            this.panel_Geral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Geral.Location = new System.Drawing.Point(0, 0);
+            this.panel_Geral.Name = "panel_Geral";
+            this.panel_Geral.Size = new System.Drawing.Size(1170, 608);
+            this.panel_Geral.TabIndex = 1;
+            // 
+            // panel_Container
+            // 
+            this.panel_Container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Container.Location = new System.Drawing.Point(203, 104);
+            this.panel_Container.Name = "panel_Container";
+            this.panel_Container.Size = new System.Drawing.Size(967, 504);
+            this.panel_Container.TabIndex = 2;
+            // 
+            // panel_BarraDeJanelas
+            // 
+            this.panel_BarraDeJanelas.BackColor = System.Drawing.Color.LightGray;
+            this.panel_BarraDeJanelas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_BarraDeJanelas.Location = new System.Drawing.Point(203, 69);
+            this.panel_BarraDeJanelas.Name = "panel_BarraDeJanelas";
+            this.panel_BarraDeJanelas.Size = new System.Drawing.Size(967, 35);
+            this.panel_BarraDeJanelas.TabIndex = 0;
+            // 
+            // panel_MenuSuperior
+            // 
+            this.panel_MenuSuperior.BackColor = System.Drawing.Color.DarkGray;
+            this.panel_MenuSuperior.Controls.Add(this.menu_Principal);
+            this.panel_MenuSuperior.Controls.Add(this.panel_BarraSuperior);
+            this.panel_MenuSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_MenuSuperior.Location = new System.Drawing.Point(203, 0);
+            this.panel_MenuSuperior.Name = "panel_MenuSuperior";
+            this.panel_MenuSuperior.Size = new System.Drawing.Size(967, 69);
+            this.panel_MenuSuperior.TabIndex = 1;
+            // 
+            // menu_Principal
+            // 
+            this.menu_Principal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.açõesToolStripMenuItem,
+            this.emitirNotaToolStripMenuItem,
+            this.receberNotaToolStripMenuItem,
+            this.cadastrosToolStripMenuItem});
+            this.menu_Principal.Location = new System.Drawing.Point(0, 28);
+            this.menu_Principal.Name = "menu_Principal";
+            this.menu_Principal.Size = new System.Drawing.Size(967, 24);
+            this.menu_Principal.TabIndex = 3;
+            this.menu_Principal.Text = "menuStrip1";
+            // 
+            // açõesToolStripMenuItem
+            // 
+            this.açõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minimizarToolStripMenuItem,
+            this.fecharToolStripMenuItem});
+            this.açõesToolStripMenuItem.Name = "açõesToolStripMenuItem";
+            this.açõesToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.açõesToolStripMenuItem.Text = "Ações";
+            // 
+            // minimizarToolStripMenuItem
+            // 
+            this.minimizarToolStripMenuItem.Name = "minimizarToolStripMenuItem";
+            this.minimizarToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.minimizarToolStripMenuItem.Text = "Minimizar";
+            // 
+            // fecharToolStripMenuItem
+            // 
+            this.fecharToolStripMenuItem.Name = "fecharToolStripMenuItem";
+            this.fecharToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.fecharToolStripMenuItem.Text = "Fechar";
+            // 
+            // emitirNotaToolStripMenuItem
+            // 
+            this.emitirNotaToolStripMenuItem.Name = "emitirNotaToolStripMenuItem";
+            this.emitirNotaToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.emitirNotaToolStripMenuItem.Text = "Emitir Nota";
+            // 
+            // receberNotaToolStripMenuItem
+            // 
+            this.receberNotaToolStripMenuItem.Name = "receberNotaToolStripMenuItem";
+            this.receberNotaToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.receberNotaToolStripMenuItem.Text = "Receber Nota";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            this.cadastrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grupoDeProdutosToolStripMenuItem,
+            this.fornecedoresToolStripMenuItem,
+            this.produtosToolStripMenuItem});
+            this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // grupoDeProdutosToolStripMenuItem
+            // 
+            this.grupoDeProdutosToolStripMenuItem.Name = "grupoDeProdutosToolStripMenuItem";
+            this.grupoDeProdutosToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.grupoDeProdutosToolStripMenuItem.Text = "Grupo de Produtos";
+            // 
+            // fornecedoresToolStripMenuItem
+            // 
+            this.fornecedoresToolStripMenuItem.Name = "fornecedoresToolStripMenuItem";
+            this.fornecedoresToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.fornecedoresToolStripMenuItem.Text = "Fornecedores";
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            // 
+            // panel_BarraSuperior
+            // 
+            this.panel_BarraSuperior.Controls.Add(this.label_MinimizarSistema);
+            this.panel_BarraSuperior.Controls.Add(this.label_FecharSistema);
+            this.panel_BarraSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_BarraSuperior.Location = new System.Drawing.Point(0, 0);
+            this.panel_BarraSuperior.Name = "panel_BarraSuperior";
+            this.panel_BarraSuperior.Size = new System.Drawing.Size(967, 28);
+            this.panel_BarraSuperior.TabIndex = 2;
+            this.panel_BarraSuperior.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_BarraSuperior_MouseDown);
+            // 
+            // label_MinimizarSistema
+            // 
+            this.label_MinimizarSistema.AutoSize = true;
+            this.label_MinimizarSistema.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_MinimizarSistema.Location = new System.Drawing.Point(904, 2);
+            this.label_MinimizarSistema.Name = "label_MinimizarSistema";
+            this.label_MinimizarSistema.Size = new System.Drawing.Size(21, 25);
+            this.label_MinimizarSistema.TabIndex = 1;
+            this.label_MinimizarSistema.Text = "-";
+            this.label_MinimizarSistema.Click += new System.EventHandler(this.label_MinimizarSistema_Click);
+            // 
+            // label_FecharSistema
+            // 
+            this.label_FecharSistema.AutoSize = true;
+            this.label_FecharSistema.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_FecharSistema.Location = new System.Drawing.Point(934, 2);
+            this.label_FecharSistema.Name = "label_FecharSistema";
+            this.label_FecharSistema.Size = new System.Drawing.Size(24, 25);
+            this.label_FecharSistema.TabIndex = 0;
+            this.label_FecharSistema.Text = "X";
+            this.label_FecharSistema.Click += new System.EventHandler(this.label_FecharSistema_Click);
+            // 
+            // panel_LogoEMenuLateral
+            // 
+            this.panel_LogoEMenuLateral.Controls.Add(this.panel_MenuLateral);
+            this.panel_LogoEMenuLateral.Controls.Add(this.panel_Logo);
+            this.panel_LogoEMenuLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_LogoEMenuLateral.Location = new System.Drawing.Point(0, 0);
+            this.panel_LogoEMenuLateral.Name = "panel_LogoEMenuLateral";
+            this.panel_LogoEMenuLateral.Size = new System.Drawing.Size(203, 608);
+            this.panel_LogoEMenuLateral.TabIndex = 0;
+            // 
+            // panel_MenuLateral
+            // 
+            this.panel_MenuLateral.BackColor = System.Drawing.Color.DarkGray;
+            this.panel_MenuLateral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_MenuLateral.Location = new System.Drawing.Point(0, 69);
+            this.panel_MenuLateral.Name = "panel_MenuLateral";
+            this.panel_MenuLateral.Size = new System.Drawing.Size(203, 539);
+            this.panel_MenuLateral.TabIndex = 2;
+            // 
+            // panel_Logo
+            // 
+            this.panel_Logo.BackColor = System.Drawing.Color.DimGray;
+            this.panel_Logo.Controls.Add(this.pictureBox1);
+            this.panel_Logo.Controls.Add(this.label1);
+            this.panel_Logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_Logo.Location = new System.Drawing.Point(0, 0);
+            this.panel_Logo.Name = "panel_Logo";
+            this.panel_Logo.Size = new System.Drawing.Size(203, 69);
+            this.panel_Logo.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe WP", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(78, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1170, 608);
+            this.Controls.Add(this.panel_Geral);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel_Geral.ResumeLayout(false);
+            this.panel_MenuSuperior.ResumeLayout(false);
+            this.panel_MenuSuperior.PerformLayout();
+            this.menu_Principal.ResumeLayout(false);
+            this.menu_Principal.PerformLayout();
+            this.panel_BarraSuperior.ResumeLayout(false);
+            this.panel_BarraSuperior.PerformLayout();
+            this.panel_LogoEMenuLateral.ResumeLayout(false);
+            this.panel_Logo.ResumeLayout(false);
+            this.panel_Logo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Geral;
+        private System.Windows.Forms.Panel panel_Container;
+        private System.Windows.Forms.Panel panel_BarraDeJanelas;
+        private System.Windows.Forms.Panel panel_MenuSuperior;
+        private System.Windows.Forms.Panel panel_LogoEMenuLateral;
+        private System.Windows.Forms.Panel panel_MenuLateral;
+        private System.Windows.Forms.Panel panel_Logo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel_BarraSuperior;
+        private System.Windows.Forms.Label label_MinimizarSistema;
+        private System.Windows.Forms.Label label_FecharSistema;
+        private System.Windows.Forms.MenuStrip menu_Principal;
+        private System.Windows.Forms.ToolStripMenuItem açõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minimizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fecharToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emitirNotaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem receberNotaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grupoDeProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fornecedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+    }
+}
+
